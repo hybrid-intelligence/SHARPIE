e6e7a4014376661c03760a8d8cbcd045bea4f2b4
Goncalves Braz <l.b.goncalvesbraz@uu.nl>
2025-09-29 23:15:49 +0200

    Adding runner and queuing system

--- webserver/experiment/admin.py
+++ webserver/experiment/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Experiment, Trial, Interaction
+from .models import Experiment, Trial, Interaction, Runner, Queue
 
 class ExperimentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'input_list', 'user_number']
+    list_display = ['name', 'input_list', 'agent_list', 'link']
 
 class TrialAdmin(admin.ModelAdmin):
     list_display = ['experiment', 'room_name', 'user', 'agent_played', 'started_at', 'ended_at']
@@ -10,6 +10,14 @@ class TrialAdmin(admin.ModelAdmin):
 class InteractionAdmin(admin.ModelAdmin):
     list_display = ['trial', 'timestamp', 'step', 'observations', 'actions', 'rewards']
 
+class RunnerAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'created_at', 'last_active', 'status', 'current_room', 'ip_address']
+
+class QueueAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'experiment', 'room_name', 'users_waiting', 'created_at', 'status']
+
 admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(Trial, TrialAdmin)
 admin.site.register(Interaction, InteractionAdmin)
+admin.site.register(Runner, RunnerAdmin)
+admin.site.register(Queue, QueueAdmin)
--- webserver/experiment/models.py
+++ webserver/experiment/models.py
@@ -5,7 +5,9 @@ class Experiment(models.Model):
     name = models.CharField('Name', max_length=100)
     description = models.TextField('Description', blank=True)
     input_list = models.JSONField('Inputs captured from the users', default=list)
-    user_number = models.IntegerField('Number of users in the same room', default=1)
+    agent_list = models.JSONField('Agents available to play', default=list(['agent_0', 'Agent']))
+    users_needed = models.IntegerField('Number of users needed to start the experiment', default=1)
+    link = models.CharField('Link to the experiment', max_length=100, blank=True)
 
     def __str__(self):
         return self.name
@@ -28,3 +30,24 @@ class Interaction(models.Model):
     observations = models.JSONField('Observation from the environment', default=dict)
     actions = models.JSONField('Action taken by the user', default=dict)
     rewards = models.JSONField('Reward received from the environment', default=dict)
+
+class Runner(models.Model):
+    created_at = models.DateTimeField('Created at', db_default=Now())
+    last_active = models.DateTimeField('Last active', db_default=Now())
+    status = models.CharField('Status', max_length=20, default='idle')
+    current_room = models.CharField('Current room being managed', max_length=20, null=True, blank=True)
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE, null=True, blank=True)
+    ip_address = models.GenericIPAddressField('IP Address', null=True, blank=True)
+
+    def __str__(self):
+        return f"Runner {self.id} - {self.experiment.name if self.experiment else 'No Experiment'}"
+    
+class Queue(models.Model):
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
+    room_name = models.CharField('Room name', max_length=20)
+    users_waiting = models.IntegerField('Number of users currently waiting', default=0) 
+    status = models.CharField('Status of the queue', max_length=20, default='waiting')  # e.g., waiting, running, terminated, dead
+    created_at = models.DateTimeField('Created at', db_default=Now())
+
+    def __str__(self):
+        return f"{self.experiment.name} - {self.room_name} ({self.users_waiting} users waiting)"
