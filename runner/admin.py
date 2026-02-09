efac6afb9c3ae71ce94a915ce355c64dfb8ac8b1
loganionian <103104454+loganionian@users.noreply.github.com>
2026-02-09 16:27:49 +0800

    Add help text in admin for experiment name matching directory

--- runner/admin.py
+++ runner/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Experiment
 
 class ExperimentAdmin(admin.ModelAdmin):
-    fields = ['name', 'description']
+    fields = ('name', 'description', 'other_fields_here')
     help_texts = {
-        'name': 'The entry name must match the experiment directory in runner/experiments.',
+        'name': 'The experiment name must match the directory name in runner/experiments.',
     }
 
 admin.site.register(Experiment, ExperimentAdmin)
