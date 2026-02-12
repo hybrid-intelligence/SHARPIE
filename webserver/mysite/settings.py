1fa3561b5bbcd96bff3f552a74540295eee3a626
Goncalves Braz <l.b.goncalvesbraz@uu.nl>
2026-02-12 10:59:52 +0100

    Add runner and data apps; remove redirect setting
    Register new 'runner' and 'data' apps in INSTALLED_APPS so their models and views are available. Remove the REDIRECT_AFTER_EXPERIMENT setting since it will be part of the Experiment settings directly.

--- webserver/mysite/settings.py
+++ webserver/mysite/settings.py
@@ -41,8 +41,6 @@ ALLOWED_HOSTS = ['*']
 # Set to False to disable new user registration
 # Set to a string value to require a registration key for new user registration
 REGISTRATION_KEY = 'sharpie'
-# Redirect URL after experiment completion for crowd sourcing platforms
-REDIRECT_AFTER_EXPERIMENT = None
 
 
 # Application definition
@@ -53,6 +51,8 @@ INSTALLED_APPS = [
     'home',
     'accounts',
     'experiment',
+    'runner',
+    'data',
 
     'crispy_forms',
     'crispy_bootstrap4',
