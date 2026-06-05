d74599e13b7b4c55083b8f951cfb3831365c3d6e
Libio Goncalves Braz <l.b.goncalvesbraz@uu.nl>
2026-06-05 11:42:21 +0200

    chore: move run_all_suites.sh to benchmark folder

--- run_all_suites.sh
+++ run_all_suites.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd "$(dirname "$0")/webserver"
+cd "../webserver"
 
-python ../benchmark/cli.py --suite ai-agents -t 50 --seed 42 --port 8123 --raw-data --format csv -k test
+python ../benchmark/cli.py --suite ai-agents -t 20 --seed 42 --port 8123 --raw-data --format csv -k test
 
-python ../benchmark/cli.py --suite network-latency -t 50 --seed 42 --port 8123 --raw-data --format csv -k test
+python ../benchmark/cli.py --suite network-latency -t 20 --seed 42 --port 8123 --raw-data --format csv -k test
 
-python ../benchmark/cli.py --suite image-size -t 50 --seed 42 --port 8123 --raw-data --format csv -k test
+python ../benchmark/cli.py --suite image-size -t 20 --seed 42 --port 8123 --raw-data --format csv -k test
 
-python ../benchmark/cli.py --suite scalability -t 50 --seed 42 --port 8123 --raw-data --format csv -k test
+python ../benchmark/cli.py --suite scalability -t 20 --seed 42 --port 8123 --raw-data --format csv -k test
 
 # Zip all merged CSV files
 cd ../benchmark/results
